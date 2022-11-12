20945d08a805e87ea24373e897d758404eadaac7
Guilherme Lacerda <lacerdagui42@gmail.com>
2022-11-12 16:20:46 -0300

    Tela Login | fontsloaded | logo add

--- mobile/App.js
+++ mobile/App.js
@@ -4,12 +4,25 @@ import { StatusBar } from 'expo-status-bar';
 
 
 import { Login } from './src/screens/Login';
+import { Cadastro } from './src/screens/Cadastro';
+
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
 
 export default function App() {
   
   const { Navigator, Screen } = createNativeStackNavigator();
 
-  return (
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    return (
       <NavigationContainer>
 
         <StatusBar
@@ -18,8 +31,10 @@ export default function App() {
         />
         <Navigator screenOptions={{headerShown: false}}>
           <Screen name="Login" component={Login}/>
-        </Navigator>
+          <Screen name="Cadastro" component={Cadastro}/>
+        </Navigator>        
 
      </NavigationContainer>
   );
+  }
 }
