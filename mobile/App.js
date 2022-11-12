3aa02ad38bbf434ba74e2dbc574e33109c47b2c5
Davi Mordonho <josedavimordonhoguimaraes@gmail.com>
2022-11-12 16:19:53 -0300

    teste merge

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Footer from './src/components/Footer';
-import { NomeDaPagina } from './src/components/NomeDaPagina';
-
-import { Perfil } from "./src/screens/Perfil";
-
+i
 export default function App() {
   return (
     <View style={styles.container}>
