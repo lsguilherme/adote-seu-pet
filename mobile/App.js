459100c28ec10560f160cab4ffe3127488303257
Davi Mordonho <josedavimordonhoguimaraes@gmail.com>
2022-11-13 15:45:36 -0300

    finalizando popup

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
-import Footer from './src/components/Footer';
-import { NomeDaPagina } from './src/components/NomeDaPagina';
-
-import { Perfil } from "./src/screens/Perfil";
 
 export default function App() {
   return (
