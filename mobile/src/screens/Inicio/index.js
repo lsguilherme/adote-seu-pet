791acf3929472af7390aeeb74f02c454cae98ef3
Guilherme Lacerda <lacerdagui42@gmail.com>
2022-11-14 14:25:52 -0300

    ajuste em nome de pasta

--- mobile/src/screens/Inicio/index.js
+++ mobile/src/screens/Inicio/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, SafeAreaView, Text, TouchableHighlight, View } from 'react-native';
+import { THEME } from '../../theme';
+
 
 import { styles } from './styles';
 
-export function Inicio() {
-  return (
-    <View style={styles.container}>
+export function Inicio({ navigation }) {
 
-    </View>
+  
+  return (
+    <SafeAreaView style={styles.screen}>
+      
+      <View style= {{alignItems: 'center'}}>
+        <Image 
+          source = {require('../../assets/Logo-Inicio.png')}
+          style = {styles.logo}
+        />
+      </View>
+      
+      <TouchableHighlight 
+        style={[
+            styles.botaoContainer, 
+            {backgroundColor: THEME.COLORS.PRIMARY}
+          ]}
+        onPress={()=>navigation.navigate('LoginECadastro',
+        {login: true})} 
+        underlayColor={THEME.COLORS.PRIMARY}
+      >
+        <Text style={styles.name}>ENTRAR</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight 
+        style={[styles.botaoContainer, {backgroundColor: THEME.COLORS.SECONDARY, marginTop: 15}]} 
+        onPress={()=>navigation.navigate('LoginECadastro', {login: false})} 
+        underlayColor={THEME.COLORS.SECONDARY}
+      >
+        <Text style={styles.name}>CADASTRAR</Text>
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
