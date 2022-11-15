009b103443b6f6060592899ae3532c395ef2ce69
Guilherme Lacerda <lacerdagui42@gmail.com>
2022-11-15 17:00:55 -0300

    tela cadastro

--- mobile/src/screens/LoginECadastro/index.js
+++ mobile/src/screens/LoginECadastro/index.js
@@ -25,7 +25,9 @@ export function LoginECadastro({ route, navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
+
       {getLogin ? (
+
         <View style={{ alignItems: "center", backgroundColor: "#F3F3F3" }}>
           <ScrollView>
             <View>
@@ -41,7 +43,7 @@ export function LoginECadastro({ route, navigation }) {
 
             <View>
               <Text style={styles.label}>E-mail</Text>
-              <TextInput style={styles.input} keyboardAppearance/>
+              <TextInput style={styles.input} />
             </View>
 
             <View>
@@ -73,8 +75,67 @@ export function LoginECadastro({ route, navigation }) {
             </TouchableOpacity>
           </ScrollView>
         </View>
+
+
       ) : (
-        <Text>Cadastro</Text>
+
+
+        <View>
+          <ScrollView>
+            <View style={{flexDirection: "row", alignItems: 'center', marginTop:50}}>
+              <View>
+                <Text style={{fontSize: THEME.FONT_SIZE.LG, fontFamily: THEME.FONT_FAMILY.SEMI_BOLD, color: THEME.COLORS.TEXT_GRAY}}>Olá! </Text>
+                <Text style={{fontSize: THEME.FONT_SIZE.LG, fontFamily: THEME.FONT_FAMILY.SEMI_BOLD, color: THEME.COLORS.TEXT_GRAY}}>Crie uma conta </Text>
+                <Text style={{fontSize: THEME.FONT_SIZE.LG, fontFamily: THEME.FONT_FAMILY.SEMI_BOLD, color: THEME.COLORS.TEXT_GRAY}}>para adotar seu novo</Text>
+                <Text style={{fontSize: THEME.FONT_SIZE.LG, fontFamily: THEME.FONT_FAMILY.SEMI_BOLD, color: THEME.COLORS.TEXT_GRAY}}>amiguinho.</Text>
+              </View>
+              <Image 
+                source={require("../../assets/Logo-Cadastro.png")}
+                style={{height:90, width:90, marginBottom:80}}
+              />
+            </View>
+
+            <View>
+              <Text style={[styles.label, {marginTop: 20}]}>Nome</Text>
+              <TextInput style={styles.input} />
+            </View>
+
+            <View>
+              <Text style={[styles.label, {marginTop: 25}]}>E-mail</Text>
+              <TextInput style={styles.input} />
+            </View>
+
+            <View>
+              <Text style={[styles.label, { marginTop: 25 }]}>Senha</Text>
+              <TextInput style={styles.input} secureTextEntry={true} />
+            </View>
+
+            <TouchableHighlight
+              style={[
+                styles.botaoContainer,
+                { backgroundColor: THEME.COLORS.PRIMARY, marginTop: 35 },
+              ]}
+              onPress={() => navigation.navigate("Inicio")}
+              underlayColor={THEME.COLORS.PRIMARY}
+            >
+              <Text style={styles.name}>CADASTRAR</Text>
+            </TouchableHighlight>
+
+            <TouchableOpacity
+              style={styles.cadastro}
+              onPress={() =>
+                navigation.navigate("LoginECadastro", { login: true })
+              }
+            >
+              <Text>Já possui conta?</Text>
+              <Text style={{ textDecorationLine: "underline" }}>
+                Clique aqui para entrar
+              </Text>
+            </TouchableOpacity>
+
+
+          </ScrollView>
+        </View>
       )}
     </SafeAreaView>
   );
