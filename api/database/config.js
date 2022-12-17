c25aa3e7b921fec88fe5e4c60f7ae3c0a59b8ed0
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-17 12:44:01 -0300

    configurando a comunicação com o banco de dados

--- api/database/config.js
+++ api/database/config.js
@@ -1,11 +1,11 @@
-import mysql from "mysql2";
+import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
-dotenv.config();
+dotenv.config()
 
-export const connect = mysql.createConnection({
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.BD_USER,
+    process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.BD_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    dialect: 'mysql'
 });
