a3d12e5c441206a118748abdf95db0de626983b9
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-21 10:01:34 -0300

    adicionando o middleware na rota de alterar e remover usuário

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,20 +1,10 @@
-import express from 'express'
-import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express';
 
 const router = express.Router();
 
-import { auth } from './middlewares/auth.js';
 import usersRoutes from "./users.js";
 //import { petsRoutes } from "./pets.js";
 
-router.get('/auth', auth, function (request, response) {
-    response.json({
-        erro: false,
-        message: 'Token válido!'
-    })
-});
 router.use('/usuarios', usersRoutes);
 //router.use('/pets', petsRoutes)
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,9 +2,17 @@ import express from 'express'
 const router = express.Router();
 
 import { getUser, getUsers, login, remove, save, update } from "../controller/users.js";
+import { auth } from './middlewares/auth.js';
 
-router.delete('/:id', remove);
-router.put('/:id', update);
+router.get('/auth', auth, function (request, response) {
+    response.json({
+        erro: false,
+        message: 'Token válido!'
+    })
+});
+
+router.delete('/:id', auth, remove);
+router.put('/:id', auth, update);
 router.post('/login', login);
 router.post('/', save);
 router.get('/:id', getUser);
