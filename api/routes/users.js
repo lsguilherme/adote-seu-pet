c554ede9b09db2360afe9902045599e3e461ebc1
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-12-28 17:17:25 -0300

    correcções nas rotas de usuário e na documentação

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,11 +4,11 @@ const router = express.Router();
 import { getUser, getUsers, login, remove, save, update, updateEmail, updateSenha } from "../controller/users.js";
 import { auth } from './middlewares/auth.js';
 
+router.post('/login', login);
 router.delete('/:id', auth, remove);
 router.put('/:id', auth, update);
 router.put('/email/:id', auth, updateEmail);
 router.put('/senha/:id', auth, updateSenha);
-router.post('/login', login);
 router.post('/', save);
 router.get('/:id', getUser);
 router.get('/', getUsers);
