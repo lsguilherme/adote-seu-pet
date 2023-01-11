58b393873817fb9395742ace07b74d5ece1fbdc4
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2023-01-11 09:25:41 -0300

    descrição das rotas

--- api/controller/users.js
+++ api/controller/users.js
@@ -5,7 +5,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getUsers = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /*
+
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Buscar todos os usuários'
+    
+    #swagger.description = '
+        Rota para buscar todos os usuários.
+    '
+    
+    #swagger.responses[200] = {
+        description: 'Usuários obtidos com sucesso!'
+    }
+    #swagger.responses[404] = {
+        description: 'Nenhum usuário encontrado!'
+    }
+
+    */
 
     operations.findAllUsers().then(results => {
         if (results.length > 0) response.send(results)
@@ -14,7 +31,25 @@ export const getUsers = (request, response) => {
 }
 
 export const getUser = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+    
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Buscar usuário por id'
+    
+    #swagger.description = '
+        Rota para buscar o usuário por id,
+        é necessário passar o id do usuário na ULR.
+    '
+    
+    #swagger.responses[200] = {
+        description: 'Usuário obtido com sucesso!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado!'
+    }
+
+    */
 
     operations.findUser(request.params.id).then(results => {
         if (results) response.send(results)
@@ -23,7 +58,28 @@ export const getUser = (request, response) => {
 }
 
 export const save = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+    
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Salvar usuário'
+    
+    #swagger.description = '
+        Rota para salvar o usuário,
+        é necessário passar o nome, o email e a senha no body.
+    '
+
+    #swagger.responses[200] = {
+        description: 'Usuário salvo com sucesso!'
+    }
+    #swagger.responses[400] = {
+        description: 'Erro na requisição!'
+    }
+    #swagger.responses[409] = {
+        description: 'Usuário já cadastrado!'
+    }
+
+    */
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(request.body.senha, salt);
@@ -39,11 +95,32 @@ export const save = (request, response) => {
             })
         }
     });
-    
+
 }
 
 export const update = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+    
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Alterar nome do usuário'
+    
+    #swagger.description = '
+        Rota para alterar o nome do usuário,
+        é necessário passar o id do usuário na ULR, o token no header e o novo nome no body.
+    '
+
+    #swagger.responses[200] = {
+        description: 'Nome do usuário alterado com sucesso!'
+    }
+    #swagger.responses[401] = {
+        description: 'Usuário não autorizado!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado!'
+    }
+    
+    */
 
     operations.update(request.params.id, { nome: request.body.nome }).then(results => {
         if (results) response.sendStatus(200)
@@ -52,7 +129,27 @@ export const update = (request, response) => {
 }
 
 export const remove = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+    
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Deletar usuário'
+    
+    #swagger.description = '
+        Rota para deletar o usuário,
+        é necessário passar o id do usuário na ULR e o token no header.
+    '
+    #swagger.responses[200] = {
+        description: 'Usuário deletado com sucesso!'
+    }
+    #swagger.responses[401] = {
+        description: 'Usuário não autorizado!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado!'
+    }
+
+    */
 
     operations.delete(request.params.id).then(results => {
         if (results) response.sendStatus(200)
@@ -61,7 +158,32 @@ export const remove = (request, response) => {
 }
 
 export const login = (request, response) => {
-    // #swagger.tags = ['Autenticação']
+
+    /* 
+    
+    #swagger.tags = ['Autenticação']
+    
+    #swagger.summary = 'Login do usuário'
+    
+    #swagger.description = '
+        Rota para realizar o login do usuário,
+        é necessário passar o email e a nova senha no body. O retorno é um json web token.
+    '
+
+    #swagger.responses[200] = {
+        description: 'Usuário autenticado com sucesso!'
+    }
+    #swagger.responses[400] = {
+        description: 'Erro na requisição!'
+    }
+    #swagger.responses[401] = {
+        description: 'Usuário não autorizado!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não emcontrado!'
+    }
+    
+    */
 
     operations.findUserByEmail(request.body.email).then(results => {
         if (results) {
@@ -72,12 +194,33 @@ export const login = (request, response) => {
                 } else response.sendStatus(401)
             })
         }
-        else response.sendStatus(400)
+        else response.sendStatus(404)
     })
 }
 
 export const updateEmail = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+    
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Alterar email do usuário'
+    
+    #swagger.description = '
+        Rota para alterar o email do usuário,
+        é necessário passar o id do usuário na ULR, o token no header e o novo email no body.
+    '
+
+    #swagger.responses[200] = {
+        description: 'Email do usuário alterado com sucesso!'
+    }
+    #swagger.responses[401] = {
+        description: 'Usuário não autorizado!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado!'
+    }
+    
+    */
 
     operations.update(request.params.id, { email: request.body.email }).then(results => {
         if (results) response.sendStatus(200)
@@ -86,7 +229,28 @@ export const updateEmail = (request, response) => {
 }
 
 export const updateSenha = (request, response) => {
-    // #swagger.tags = ['Usuários']
+    /* 
+
+    #swagger.tags = ['Usuários']
+    
+    #swagger.summary = 'Alterar senha do usuário'
+    
+    #swagger.description = '
+        Rota para alterar a senha do usuário,
+        é necessário passar o id do usuário na ULR, o token no header e a nova senha no body.
+    '
+
+    #swagger.responses[200] = {
+        description: 'Senha do usuário alterada com sucesso!'
+    }
+    #swagger.responses[401] = {
+        description: 'Usuário não autorizado!'
+    }
+    #swagger.responses[404] = {
+        description: 'Usuário não encontrado!'
+    }
+    
+    */
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(request.body.senha, salt);
