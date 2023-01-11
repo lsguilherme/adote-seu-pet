2d5c9fd2568dc094f43d769bcf68802349d43a4a
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2023-01-10 21:52:44 -0300

    correção nas variáveis de ambiente

--- api/database/config.js
+++ api/database/config.js
@@ -4,9 +4,10 @@ dotenv.config()
 
 export const sequelize = new Sequelize(
     process.env.DB_DATABASE,
-    process.env.BD_USER,
+    process.env.DB_USER,
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     timezone: "-3:00",
     pool: {
